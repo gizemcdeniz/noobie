263d0619e05fb0ab39ac11368c3a47e31d9bbf8b
Onur Cem EZGİN <cemezgn@gmail.com>
2020-04-03 21:43:31 +0300

    Update gizem.php

--- Pdo integration and getting data/gizem.php
+++ Pdo integration and getting data/gizem.php
@@ -1,14 +1,29 @@
+<html>
+<body>
+
+<form action="gizem.php" method="post">
+Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
 <?php
 
 include ("mysql.php");
-
-$query = $conn->query("SELECT * FROM user", PDO::FETCH_ASSOC);
- if ( $query->rowCount() ){
-  foreach( $query as $row ){
-  print $row['username']."<br />";
-  print $row['password']."<br />";
-  print $row['phone']."<br />";
- }
-}
+ $query = $pdo->prepare("SELECT password FROM user WHERE username=?");
+    $query->execute([$_POST['username']]);
+    if($query->fetchColumn() === $_POST['password']) 
+    {
+      if ( $query->rowCount() ){
+        foreach( $query as $row ){
+          print $row['username']."<br />";
+          print $row['password']."<br />";
+          print $row['phone']."<br />";
+        }
+      }
+    }
 
 ?>
